3e783b7f68240a8d476f8e674223e4467e2c3323
Diego Porto <diego.cfporto@gmail.com>
2020-01-29 21:42:16 -0300

    desenhando tela de login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,13 +5,65 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("MPI Brasil"),
-        ),
-        body: Center(
-          child: Text("Login")
+    // username field properties
+    final usernameField = TextField(
+      obscureText: false,
+      decoration: InputDecoration(
+        hintText: "Usuário",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+        contentPadding: EdgeInsets.all(15),
+      ),
+    );
+
+    // password field properties
+    final passwordField = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Senha",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+        contentPadding: EdgeInsets.all(15),
+      ),
+    );
+
+    // login button properties
+    final loginButton = Material(
+        elevation: 2.0,
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(30),
+        child: MaterialButton(
+          onPressed: () {},
+          padding: EdgeInsets.all(15),
+          child: Text(
+            "Entrar",
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
         )
+    );
+
+    return Scaffold(
+        body: Center(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(35.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset("assets/images/logo.png", fit: BoxFit.contain),
+                  SizedBox(height: 50.0),
+                  usernameField,
+                  SizedBox(height: 20.0),
+                  passwordField,
+                  SizedBox(height: 30.0),
+                  loginButton,
+                  SizedBox(height: 30.0),
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
