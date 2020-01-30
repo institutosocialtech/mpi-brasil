c8d5aa003427997742efd9c4f537c46c4b1d5a80
Diego Porto <diego.cfporto@gmail.com>
2020-01-30 04:51:08 -0300

    home-screen: redesenhando a tela

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,80 @@
 import 'package:flutter/material.dart';
+import 'medicamentos.dart';
+import 'glossario.dart';
+import 'info.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState(){
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    Info(),
+    Medicamentos(),
+    Glossario(),
+  ];
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
         appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Image.asset(
+              'assets/icons/health.png',
+              fit: BoxFit.contain
+            ),
+          ),
           title: Text("MPI Brasil"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              }
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                Navigator.pushNamed(context, '/settings');
+               },
+            ),
+          ],
         ),
-        body: Center(
-          child: Text("Home")
-        )
+        body: _children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black45,
+          currentIndex: _currentIndex,
+          onTap: onTabTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/health.png', height: 40),
+              title: Text("Home", style: TextStyle(fontSize: 14)),
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/drug.png', height: 40),
+              title: Text("Medicamentos", style: TextStyle(fontSize: 14)),
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/agenda.png', height: 40),
+              title: Text("Glossário", style: TextStyle(fontSize: 14)),
+            ),
+          ],
+        ),
     );
+  }
+
+  void onTabTapped(int index){
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
