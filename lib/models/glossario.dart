6b1312c36059ebf80c6a75c832ca8a3f9229a30b
Diego Porto <diego.cfporto@gmail.com>
2020-02-18 18:53:21 -0300

    mockup inicial do glossário

---
+++ lib/models/glossario.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class GlossarioItem {
+  final String name;
+  final String description;
+
+  GlossarioItem({
+    @required this.name,
+    @required this.description,
+  });
+
+}
