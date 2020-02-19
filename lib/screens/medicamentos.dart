baa19bdbc5ed34a86def0a582e7ae80cdfd73e65
Diego Porto <diego.cfporto@gmail.com>
2020-02-19 17:34:09 -0300

    redesenhando tela de medicamentos com barra de pesquisa e de filtros

--- lib/screens/medicamentos.dart
+++ lib/screens/medicamentos.dart
@@ -19,21 +19,62 @@ class Medicamentos extends StatelessWidget {
     }
 
     return Scaffold(
-        body: ListView.separated(
-          itemCount: medList.length,
-          separatorBuilder: (BuildContext context, int index) => Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              contentPadding: EdgeInsets.symmetric(horizontal: 16),
-              title: Row(children: <Widget>[
-                Text(medList[index].name),
-                IconButton(icon: Icon(Icons.star_border), color: Colors.orangeAccent, iconSize: 16, padding: EdgeInsets.all(0), onPressed: () {})
-              ]),
-              subtitle: Text(medList[index].description),
-              trailing: IconButton(icon: Icon(Icons.keyboard_arrow_right), color: Colors.black26, onPressed: () {}),
-            );
-          },
-        ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.rectangle,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: Icon(Icons.mic),
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: "Pesquisar por Medicamento",
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("Tipo"),
+                Text("Nome", style: TextStyle(fontWeight: FontWeight.bold), ),
+              ]
+            ),
+          ),
+          Container(
+            height: 540,
+            child: ListView.separated(
+              itemCount: medList.length,
+              separatorBuilder: (BuildContext context, int index) => Divider(),
+              itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                    title: Row(children: <Widget>[
+                      Text(medList[index].name),
+                      IconButton(icon: Icon(Icons.star_border), color: Colors.orangeAccent, iconSize: 16, padding: EdgeInsets.all(0), onPressed: () {})
+                    ]),
+                    subtitle: Text(medList[index].description),
+                    trailing: IconButton(icon: Icon(Icons.keyboard_arrow_right), color: Colors.black26, onPressed: () {}),
+                  );
+                },
+              ),
+            ),
+          ],
+      ),
     );
   }
 }
