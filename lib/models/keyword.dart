ca7f14f29b3dbff0948680389dd2e2c8b0aca9ca
Diego Porto <diego.cfporto@gmail.com>
2020-04-25 15:46:18 -0300

    fix empty synonym list errors

--- lib/models/keyword.dart
+++ lib/models/keyword.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/foundation.dart';
-import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:mpibrasil/providers/keywords.dart';
 
 part 'keyword.g.dart';
 
-@JsonSerializable(explicitToJson: true)
+@JsonSerializable(explicitToJson: true, nullable: true)
 class Keyword with ChangeNotifier {
   final String id;
   final String word;
-  final List<String> synonyms; 
+  final List<String> synonyms;
   final String definition;
   final String source;
 
-  Keyword(
+  Keyword({
     this.id,
     this.word,
     this.synonyms,
     this.definition,
-    this.source
-  );
+    this.source,
+  });
 
-  factory Keyword.fromJson(Map<String, dynamic> json) => _$KeywordFromJson(json);
+  factory Keyword.fromJson(Map<String, dynamic> json) {
+    Keyword a = _$KeywordFromJson(json);
+
+    if (a.synonyms.length == 1 && a.synonyms[0].length == 0) {
+      List<String> emptyList = [];
+      return Keyword(
+        id: a.id,
+        word: a.word,
+        synonyms: emptyList,
+        definition: a.definition,
+        source: a.source,
+      );
+    }
+
+    return a;
+  }
 
   Map<String, dynamic> toJson() => _$KeywordToJson(this);
-
-  String printJson() {
-    var jmap = _$KeywordToJson(this);
-    return jsonEncode(jmap);
-  }
 
   String synonymsListToString() {
     String output = "";
