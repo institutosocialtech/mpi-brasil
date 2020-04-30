c7e38b7b89e77dfeccb199b3220b2b1c3944e379
Diego Porto <diego.cfporto@gmail.com>
2020-04-30 19:08:17 -0300

    auth: initial authentication support and login screen rework

---
+++ lib/providers/auth.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expirationDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    const apiKey = '';
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey';
+
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+}
---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth.dart';
+
+class AuthScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.green[900],
+                  Colors.green[200],
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0, 1],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              height: deviceSize.height,
+              width: deviceSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 60),
+                      child: Text(
+                        "MPI Brasil",
+                        textScaleFactor: 3,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: deviceSize.width > 600 ? 2 : 1,
+                    child: AuthCard(),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AuthCard extends StatefulWidget {
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final _passwordController = TextEditingController();
+  var _isLoading = false;
+
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      // invalid
+      return;
+    }
+
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+
+    await Provider.of<Auth>(context, listen: false).login(
+      _authData['email'],
+      _authData['password'],
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  String validateEntry(String type, String value) {
+    switch (type) {
+      case 'email':
+        if (value.isEmpty || !value.contains('@')) return 'Email Inválido';
+        break;
+
+      case 'password':
+        if (value.isEmpty) return 'Digite sua Senha!';
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return _isLoading
+        ? CircularProgressIndicator(
+            backgroundColor: Colors.white,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.green[800]),
+          )
+        : Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 8,
+            child: Container(
+              height: 360,
+              constraints: BoxConstraints(minHeight: 360),
+              width: deviceSize.width * 0.85,
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    // username and pass section
+                    Column(
+                      children: <Widget>[
+                        TextFormField(
+                          obscureText: false,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
+                          ),
+                          validator: (value) => validateEntry('email', value),
+                          onSaved: (value) => _authData['email'] = value,
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          obscureText: true,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: "Senha",
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
+                          ),
+                          validator: (value) =>
+                              validateEntry('password', value),
+                          onSaved: (value) => _authData['password'] = value,
+                        ),
+                      ],
+                    ),
+
+                    // signIn button section
+                    Column(children: <Widget>[
+                      SizedBox(height: 20),
+                      RaisedButton(
+                        child: Text("Entrar"),
+                        onPressed: _submit,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text("Primeiro acesso? Cadastre aqui!"),
+                      Text("Esqueci minha senha"),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
+}
