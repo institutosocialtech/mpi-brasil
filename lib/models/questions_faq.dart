9284b180a0609f2803b246cf2c5d6cfb397dd466
Yago C Martins <yagocunhamartins@gmail.com>
2020-05-02 13:12:41 -0300

    Criação tela About e subtelas do menu

---
+++ lib/models/questions_faq.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+class QuestionsFaq {
+  QuestionsFaq(this.title, this.subtitle);
+
+  final String title;
+  final String subtitle;
+}
+
+final List<QuestionsFaq> data = [
+  QuestionsFaq('O que é Medicamentos Inapropriados para Idosos (MPI)?',
+      'Um medicamento é considerado potencialmente inapropriado para idosos quando apresenta um risco significativo de evento adverso na evidência de alternativa igual ou mais efetiva e com menor risco para tratar a mesma condição.'),
+  QuestionsFaq('O que são as Reações Adversas a Medicamentos (RAM)?',
+      "Na assistência clínica ao paciente idoso, é importante ficar atento ao uso de medicamentos, pois as Reações Adversas a Medicamentos (RAM) é a forma mais comum de latrogenia nos idosos.\n Muitos fármacos comumente prescritos levam a RAM potencialmente ameaçadoras a vida ou incapacitantes."),
+  QuestionsFaq('Como prescrever um medicamento apropriado?',
+      'A escolha do medicamento apropriado para cada doença em particular é um processo complexo, pois é essencial que a prescrição seja clinicamente efetiva, segura e tenha uma relação de custo=benefício satisfatória.'),
+  QuestionsFaq(
+      'O que é o Consenso Brasileiro de Medicamentos Potencialmente Inapropriados para Idosos?',
+      'É um instrumento para detecção de MPI adaptados à realidade brasileira, divide estes medicamentos em 2 classes: \n MPI independente de condição clínica \n MPI a depender de condição clínica'),
+  QuestionsFaq('O App é Open Source?', 'O App MPI Brasil foi desenvolvido ...'),
+  QuestionsFaq('Como posso contribuir?', 'Conteúdo: \n Sugerir Informação \n Sugerir Correção \n Adicionar Medicamento \n Outras Informações fale conosco por e-mail\n\n Aplicação: \n Acesse nosso GitHub,'),
+];
--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,17 +1,209 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mailer/flutter_mailer.dart';
+import 'package:mpibrasil/screens/terms_use.dart';
+import 'package:mpibrasil/screens/privacy_policy.dart';
+import 'package:mpibrasil/screens/license.dart';
 
 class About extends StatelessWidget {
-
+  final medTitleStyle =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MPI Brasil'),
-          titleSpacing: 0.0,
-        ),
-        body: Center(
-          child: Text("Sobre")
-        )
+      appBar: AppBar(
+        title: Text('MPI Brasil'),
+        titleSpacing: 0.0,
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.green,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Sobre", textScaleFactor: 1.5, style: medTitleStyle),
+              ],
+            ),
+          ),
+          Expanded(
+            child: AboutList(),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class AboutList extends StatelessWidget {
+  final medTitleStyle =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        ListTile(
+            title: Text(
+              'App MPI Brasil',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onTap: () => showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Container(
+                      color: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text("O que é o App MPI Brasil",
+                              style: medTitleStyle),
+                        ],
+                      ),
+                    ),
+                    // Text("O que é o App MPI Brasil"),
+                    content: Text(
+                      " É um instrumento de busca rápida sobre os MPI disponíveis no Brasil, auxiliando o médico no momento da tomada de decisão sobre qual medicamento seria mais apropriado para o paciente idoso. Além de listar os medicamentos considerados MPI, o aplicativo traz informações se o medicamento é MPI independente de condição clínica, se sim, qual o racional para isto e as situações de uso tidas como exceções. Cita se há alguma condição clínica específica que o medicamento deve ser evitado, quais as alternativas terapêuticas mais apropriadas disponíveis e, quando um medicamento não pode ser suspenso subitamente, dá as orientações de desmame (desprescrição). Além disso, caso seja necessário utilizar o MPI, o aplicativo fornece as orientações de monitoramento para tornar o uso do MPI mais seguro. Por fim, as referências bibliográficas utilizadas para cada MPI são disponibilizadas. Há também um glossário de termos médicos utilizados no aplicativo para melhor compreensão.",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  );
+                })),
+        Divider(),
+        ListTile(
+          title: Text(
+            'Versão',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            "2.1",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+        Divider(),
+        ListTile(
+          title: Text(
+            'Licenças',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.keyboard_arrow_right),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => License(),
+                  ),
+                );
+              }),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => License(),
+              ),
+            );
+          },
+        ),
+        Divider(),
+        ListTile(
+          title: Text(
+            'Termo de Uso',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.keyboard_arrow_right),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TermsUse(),
+                  ),
+                );
+              }),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TermsUse(),
+              ),
+            );
+          },
+        ),
+        Divider(),
+        ListTile(
+          title: Text(
+            'Política de Privacidade',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.keyboard_arrow_right),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PrivacyPolicy(),
+                  ),
+                );
+              }),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PrivacyPolicy(),
+              ),
+            );
+          },
+        ),
+        Divider(),
+        ListTile(
+          title: Text(
+            'Fale Conosco',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.keyboard_arrow_right),
+              color: Colors.grey,
+              onPressed: () {
+                final MailOptions mailOptions = MailOptions(
+                  body:
+                      ' Conte-nos a sua opinião, ela é muito importante para nós!',
+                  subject: "Fale Conosco",
+                  recipients: ['mpibrasil@pmosocial.org'],
+                  isHTML: true,
+                );
+
+                FlutterMailer.send(mailOptions);
+              }),
+          onTap: () {
+            final MailOptions mailOptions = MailOptions(
+              body:
+                  ' Conte-nos a sua opinião, ela é muito importante para nós!',
+              subject: "Fale Conosco",
+              recipients: ['mpibrasil@pmosocial.org'],
+              isHTML: true,
+            );
+
+            FlutterMailer.send(mailOptions);
+          },
+        ),
+        Divider(),
+      ],
+    ));
   }
 }
