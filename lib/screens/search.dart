2585dee0246c0e307467432815121794e3d25bb0
Daniel Porto <daniel.porto@gmail.com>
2020-05-15 14:15:55 +0100

    Rewrite app bar including the search text field

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,40 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:mpibrasil/widgets/drawer.dart';
+import 'package:provider/provider.dart';
+import '../models/med.dart';
+import '../providers/meds.dart';
+import '../screens/med_details.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Meds>(context).fetchMedsFromDB().then((value) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('MPI Brasil'),
         titleSpacing: 0.0,
         elevation: 0,
-      ),
-      body: Column( children: [Container(
-        height: 80,
-        color: Colors.green,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(10),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-              suffixIcon: Icon(Icons.search),
-              fillColor: Colors.white,
-              filled: true,
-              hintText: "Pesquisar...",
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(80),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                suffixIcon: Icon(Icons.search),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Pesquisar...",
+              ),
             ),
           ),
         ),
       ),
-        Expanded(child: Container(
-          child: Image.asset("assets/images/logo.jpg"),
-        ))]),
+      body: Container(
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Center(child: Image.asset("assets/images/logo.jpg")),
+      ),
       drawer: AppDrawer(),
     );
+  }
+}
 
+class MPISearch extends SearchDelegate<Med> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [Container()];
+  }
 
+  @override
+  Widget buildLeading(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
   }
 }
