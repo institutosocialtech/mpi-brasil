b3b0ef1070a43e0e1a3e130bc40220eac02b614d
Diego Porto <diego.cfporto@gmail.com>
2021-09-12 14:07:47 -0300

    constants: add static app values for colors and layouts

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// colors
+const Color kColorMPIGreen = Color(0xFF6DB31E);
+const Color kColorMPIBlue = Color(0xFF1E5BB3);
+const Color kColorMPIGray = Color(0xFF8F9BB3);
+const Color kColorMPIWhite = Color(0xFFFFFFFF);
+
+// layout
+const double kAppBarElevation = 0.0;
+const double kDrawerBorderRadius = 20.0;
+
+// buttons
+const double kButtonBorderRadius = 4.0;
+
+// cards
+const double kCardBorderRadius = 5.0;
+const double kCardElevation = 5.0;
+
+// textfield
+const double kInputBorderRadius = 4.0;
+
+// font sizes
+// header: 22
+// text: 20
+// input and buttons: 18
+// navButton: 15
