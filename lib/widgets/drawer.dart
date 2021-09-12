8ec54bb971e14c3645c45b9a29d8b2032fdcc880
Diego Porto <diego.cfporto@gmail.com>
2021-09-12 14:08:09 -0300

    drawer: move logout button to the bottom and add rounded borders

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,21 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
+import '../constants.dart';
 import '../providers/auth.dart';
 import '../providers/userpreferences.dart';
 
 class AppDrawer extends StatelessWidget {
+  void _logout(BuildContext context) async {
+    var headerStyle = TextStyle(
+      color: kColorMPIGray,
+      fontSize: 17,
+      fontWeight: FontWeight.bold,
+    );
+
+    var messageStyle = TextStyle(
+      color: kColorMPIGray,
+      fontWeight: FontWeight.normal,
+    );
+
+    final action = await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          elevation: 24,
+          title: Text('Sair', style: headerStyle),
+          content: Text('Deseja efetuar o logout?', style: messageStyle),
+          actionsPadding: EdgeInsets.all(4.0),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text('Não'),
+              style: TextButton.styleFrom(
+                primary: kColorMPIGray,
+                backgroundColor: kColorMPIWhite,
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('Sim'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: kColorMPIGreen,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (action) {
+      Navigator.pop(context);
+      Navigator.pushReplacementNamed(context, '/');
+      Provider.of<Auth>(context, listen: false).logout();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          _buildDrawerHeader(context),
-          _buildUserTile(context),
-          Divider(),
-          _buildDrawerList(context),
-        ],
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(kDrawerBorderRadius),
+        bottomRight: Radius.circular(kDrawerBorderRadius),
+      ),
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            _buildDrawerHeader(context),
+            _buildUserTile(context),
+            Divider(indent: 20, endIndent: 20),
+            _buildDrawerList(context),
+            Divider(indent: 20, endIndent: 20),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              onTap: () => _logout(context),
+              title: Text('Sair'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -39,48 +102,8 @@ class AppDrawer extends StatelessWidget {
     var _userName = _userPrefs.user.name;
 
     return ListTile(
-      title: Text(_userName),
+      title: Text('Olá, $_userName'),
       onTap: () => Navigator.of(context).popAndPushNamed('/profile'),
-      trailing: GestureDetector(
-        child: Icon(Icons.exit_to_app),
-        onTap: () async {
-          final action = await showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                elevation: 24,
-                title: Text('Sair'),
-                content: Text('Deseja efetuar o logout?'),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(false),
-                    child: Text('Não'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      backgroundColor: Colors.green,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(true),
-                    child: Text('Sim'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      backgroundColor: Colors.green,
-                    ),
-                  ),
-                ],
-              );
-            },
-          );
-
-          if (action) {
-            Navigator.pop(context);
-            Navigator.pushReplacementNamed(context, '/');
-            Provider.of<Auth>(context, listen: false).logout();
-          }
-        },
-      ),
     );
   }
 
