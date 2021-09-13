c60d8acdf227723fe6408e520c4fc32cd26793bb
Diego Porto <diego.cfporto@gmail.com>
2021-09-12 21:26:17 -0300

    keyword_details: update page layout

--- lib/screens/keyword_details.dart
+++ lib/screens/keyword_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/keyword.dart';
+import '../constants.dart';
 
 class KeywordDetails extends StatelessWidget {
   final Keyword keyword;
@@ -8,33 +9,80 @@ class KeywordDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final headerStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    );
+
     return Scaffold(
+      backgroundColor: kColorMPIGreenOpaque,
       appBar: AppBar(
-        title: Text('MPI Brasil'),
-        titleSpacing: 0.0,
-        elevation: 0,
+        backgroundColor: kColorMPIGreen,
+
+        // page appbar
+        flexibleSpace: Container(
+          child: Image.asset(
+            'assets/images/med_composition.png',
+            color: Colors.white.withOpacity(0.15),
+            colorBlendMode: BlendMode.multiply,
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // page title
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(80),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              keyword.word.toUpperCase(),
+              style: headerStyle,
+              overflow: TextOverflow.fade,
+            ),
+          ),
+        ),
       ),
-      body: ListView(
-        children: <Widget>[
-          drawTitleBar(keyword),
-          SizedBox(height: 20),
-          ListTile(
-            title: Text("Definição", style: tileTitle),
-            subtitle: Text(keyword.definition, textAlign: TextAlign.justify,),
+
+      // page content
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: kColorMPIWhite,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15.0),
+            topRight: Radius.circular(15.0),
           ),
-          keyword.synonyms == null
-            ? Container()
-            : ListTile(
-              title: Text("Sinônimos", style: tileTitle),
-              subtitle: Text(keyword.synonymsListToString(), textAlign: TextAlign.justify,),
-          ),
-          ListTile(
-            title: Text("Referências", style: tileTitle),
-            subtitle: keyword.source == null
-                ? Text("Indisponível")
-                : Text(keyword.source),
-          ),
-        ],
+        ),
+        child: ListView(
+          children: <Widget>[
+            // drawTitleBar(keyword),
+            SizedBox(height: 20),
+            ListTile(
+              title: Text("Definição", style: tileTitle),
+              subtitle: Text(
+                keyword.definition,
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            keyword.synonyms == null
+                ? Container()
+                : ListTile(
+                    title: Text("Sinônimos", style: tileTitle),
+                    subtitle: Text(
+                      keyword.synonymsListToString(),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+            ListTile(
+              title: Text("Referências", style: tileTitle),
+              subtitle: keyword.source == null
+                  ? Text("Indisponível")
+                  : Text(keyword.source),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -42,8 +90,10 @@ class KeywordDetails extends StatelessWidget {
 //
   // Keyword Title Bar
   Widget drawTitleBar(Keyword keyword) {
-    TextStyle headerStyle =
-        TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
+    TextStyle headerStyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    );
 
     return Container(
       height: 80,
