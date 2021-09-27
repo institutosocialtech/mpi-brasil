f57c9c697bef39d0071fc5df19b9bc42a458852c
Diego Porto <diego.cfporto@gmail.com>
2021-09-27 00:24:53 -0300

    theme: set proper accent color

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primaryColor: kColorMPIGreen,
-            accentColor: kColorMPIWhite,
+            accentColor: kColorMPIGreenOpaque,
             fontFamily: 'Nunito',
 
             // app bar
