ade8b5863826d3d2a7ae37792d7725de521fa980
Diego Porto <diego.cfporto@gmail.com>
2021-10-20 02:54:43 -0300

    splashscreen: update layout

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,25 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import '../constants.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.green[900],
-            Colors.green[200],
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg_shadow.png'),
+            colorFilter: ColorFilter.mode(kColorMPIGreen, BlendMode.multiply),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // header
+            SizedBox(height: 50),
+
+            // logo and progress indicator
+            SvgPicture.asset('assets/images/group3.svg'),
+
+            // footer
+            Column(
+              children: [
+                // progress indicator
+                Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: kColorMPIWhite,
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(kColorMPIGreenOpaque),
+                  ),
+                ),
+
+                Container(
+                  height: 50,
+                  alignment: Alignment.bottomCenter,
+                  child: FutureBuilder<PackageInfo>(
+                    // use future builder to pull package info
+                    future: PackageInfo.fromPlatform(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        // draw app version
+                        case ConnectionState.done:
+                          return Text(
+                            'v${snapshot.data.version}',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .caption
+                                .copyWith(color: kColorMPIWhite),
+                          );
+
+                        // draw empty text
+                        default:
+                          return Text('');
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          stops: [0, 1],
         ),
       ),
-      child: Center(
-          child: CircularProgressIndicator(
-        backgroundColor: Colors.white,
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.green[800]),
-      )),
     );
   }
 }
