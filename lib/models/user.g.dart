74c8998b8ed8e887c0a2555a757edc4625f1469f
Diego Porto <diego.cfporto@gmail.com>
2024-08-31 19:09:17 -0300

    deps: update JsonSerializable generated classes

--- lib/models/user.g.dart
+++ lib/models/user.g.dart
@@ -6,20 +6,18 @@ part of 'user.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-User _$UserFromJson(Map<String, dynamic> json) {
-  return User(
-    id: json['id'] as String,
-    name: json['name'] as String,
-    occupation: json['occupation'] as String,
-    birthDate: json['birth_date'] == null
-        ? null
-        : DateTime.parse(json['birth_date'] as String),
-    favorites: (json['favorites'] as Map<String, dynamic>)?.map(
-          (k, e) => MapEntry(k, e as bool),
-        ) ??
-        {},
-  );
-}
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as String,
+      name: json['name'] as String? ?? '',
+      occupation: json['occupation'] as String? ?? '',
+      birthDate: json['birth_date'] == null
+          ? null
+          : DateTime.parse(json['birth_date'] as String),
+      favorites: (json['favorites'] as Map<String, dynamic>?)?.map(
+            (k, e) => MapEntry(k, e as bool),
+          ) ??
+          {},
+    );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
