04c459b357ab276c9181e8d22318379a4f54189c
Diego Porto <diego.cfporto@gmail.com>
2024-08-31 16:30:01 -0300

    fix: remove unused svg package on `SearchPage`

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -1,7 +1,6 @@
 import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_svg/svg.dart';
 
 import '../../providers/meds.dart';
 import '../../providers/userpreferences.dart';
