5e605b9be6172bb7e22c600aca9df73607052461
Diego Porto <diego.cfporto@gmail.com>
2024-08-31 18:44:59 -0300

    fix: refactor `User` model to enforce null safety

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,21 +3,21 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'user.g.dart';
 
-@JsonSerializable(explicitToJson: true, nullable: true)
+@JsonSerializable(explicitToJson: true)
 class User with ChangeNotifier {
   @JsonKey(name: 'id')
   String id;
-  @JsonKey(name: 'name')
-  String name;
-  @JsonKey(name: 'occupation')
-  String occupation;
-  @JsonKey(name: 'birth_date')
-  DateTime birthDate;
+  @JsonKey(name: 'name', defaultValue: '')
+  String? name;
+  @JsonKey(name: 'occupation', defaultValue: '')
+  String? occupation;
+  @JsonKey(name: 'birth_date', defaultValue: null)
+  DateTime? birthDate;
   @JsonKey(name: 'favorites', defaultValue: {})
-  Map<String, bool> favorites;
+  Map<String, bool>? favorites;
 
   User({
-    this.id,
+    required this.id,
     this.name,
     this.occupation,
     this.birthDate,
@@ -26,5 +26,4 @@ class User with ChangeNotifier {
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
-
 }
