2a2327845b0b19dd2c7a2eb2cf1e0ecf9e025e69
Diego Porto <diego.cfporto@gmail.com>
2024-09-29 18:11:45 -0300

    feat: add localization to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mpibrasil/models/user.dart';
 import 'package:mpibrasil/screens/profile/delete_account.dart';
 import 'package:provider/provider.dart';
@@ -63,6 +64,8 @@ class MyApp extends StatelessWidget {
         builder: (context, auth, _) => MaterialApp(
           title: 'MPI Brasil',
           debugShowCheckedModeBanner: false,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
           theme: appTheme,
           home: auth.isAuth
               ? LoadingScreen()
