d04a4f0b8be89a268b83c525bc667693a7028049
Diego Porto <diego.cfporto@gmail.com>
2024-10-05 16:53:11 -0300

    fix: use flutter_intl for localization

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-/// add localization strings to build context
-extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
-}
 
 extension DateTimeX on DateTime {
   String get formattedDate => '${this.day}/${this.month}/${this.year}';
