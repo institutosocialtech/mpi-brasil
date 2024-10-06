456dc0c19f93db6f0a38892f1fbc6a6cf8ea48c2
Diego Porto <diego.cfporto@gmail.com>
2024-10-05 22:30:37 -0300

    l10n: onboarding strings and assets

--- lib/assets.dart
+++ lib/assets.dart
@@ -6,4 +6,10 @@ class MpiAssets {
 
   // search page
   static String imgMedComposition = 'assets/images/med_composition.png';
+
+  // onboarding page
+  static String imgUndrawDoctors = 'assets/undraw/doctors.png';
+  static String imgUndrawMedicalCare = 'assets/undraw/medical_care.png';
+  static String imgUndrawMedicine = 'assets/undraw/medicine.png';
+  static String imgUndrawQuestions = 'assets/undraw/questions.png';
 }
