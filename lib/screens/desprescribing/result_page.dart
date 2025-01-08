777d59d0dfd50fc68953d8ed94bc6d033845bf60
Diego Porto <diego.cfporto@gmail.com>
2025-01-08 11:22:32 -0300

    fix: cleanup `ResultPage`

--- lib/screens/desprescribing/result_page.dart
+++ lib/screens/desprescribing/result_page.dart
@@ -4,80 +4,83 @@ import 'package:material_symbols_icons/symbols.dart';
 import 'package:mpibrasil/assets.dart';
 import 'package:mpibrasil/constants.dart';
 import 'package:mpibrasil/generated/l10n.dart';
+import 'package:mpibrasil/screens/desprescribing/result_card.dart';
 
 class DespResultPage extends StatelessWidget {
-  final Color bgColor;
-  final String resultTitle;
-  final String resultContentList;
-
-  const DespResultPage({
-    Key? key,
-    required this.bgColor,
-    required this.resultTitle,
-    required this.resultContentList,
-  }) : super(key: key);
+  const DespResultPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final contentList = resultContentList.split('\$');
-
-    final contentWidgets = contentList.map((entry) {
-      return ListTile(
-        leading: Icon(Symbols.check_rounded, color: kColorMPIGreen),
-        title: Text(entry),
-        titleTextStyle: Theme.of(context)
-            .textTheme
-            .bodyLarge!
-            .copyWith(color: Colors.black),
-      );
-    }).toList();
+    final headerStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    );
 
     return Scaffold(
-      backgroundColor: Colors.white,
+      // scaffold appbar
+      appBar: AppBar(
+        backgroundColor: kColorMPIGreenOpaque,
+        title: Text('Resultado', style: headerStyle),
+        centerTitle: true,
+      ),
+      // scaffold bg color
+      backgroundColor: kColorMPIGreenOpaque,
+      // scaffold body
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
             padding: const EdgeInsets.all(10.0),
             child: Column(
               children: [
-                // result image
+                // svg undraw
                 Padding(
                   padding: const EdgeInsets.only(bottom: 25.0),
                   child: SvgPicture.asset(
-                    MpiAssets.svgGroup3,
-                    height: size.height * 0.10,
-                    color: kColorMPIGreen,
+                    MpiAssets.svgUndrawMedicalCare,
+                    height: size.height * 0.20,
+                    // colorFilter: ColorFilter.mode(
+                    //   kColorMPIGreenOpaque,
+                    //   BlendMode.colorBurn,
+                    // ),
                   ),
                 ),
 
-                // result title
                 Padding(
                   padding: const EdgeInsets.only(bottom: 25.0),
-                  child: Text(
-                    resultTitle,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineMedium!
-                        .copyWith(fontWeight: FontWeight.bold),
+                  child: ResultCard(
+                    title: S.current.considerDeprescribingTitle,
+                    iconData: Symbols.pill_off,
+                    iconColor: kColorMPIRed,
+                    contentList:
+                        S.current.considerDeprescribingList.split('\$'),
                   ),
                 ),
-
-                // result content
-                ...contentWidgets,
-
-                // footer
                 Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: Text(S.current.close.toUpperCase()),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: kColorMPIGreen,
-                      foregroundColor: kColorMPIWhite,
-                      textStyle: TextStyle(fontWeight: FontWeight.bold),
-                    ),
+                  padding: const EdgeInsets.only(bottom: 25.0),
+                  child: ResultCard(
+                    title: S.current.considerNotDeprescribingTitle,
+                    iconData: Symbols.pill,
+                    iconColor: kColorMPIGreen,
+                    contentList:
+                        S.current.considerNotDeprescribingList.split('\$'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 25.0),
+                  child: ResultCard(
+                    isNumbered: true,
+                    title: S.current.howToDeprescribeTitle,
+                    contentList: S.current.howToDeprescribeList.split('\$'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 25.0),
+                  child: ResultCard(
+                    isNumbered: true,
+                    title: S.current.howToMonitorTitle,
+                    contentList: S.current.howToMonitorList.split('\$'),
                   ),
                 ),
               ],
