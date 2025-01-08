8d7478edafa6278b293c4eb9156364f89b97e1dd
Diego Porto <diego.cfporto@gmail.com>
2025-01-08 08:36:31 -0300

    fix: refactor desprescribing main page

--- lib/screens/desprescribing/desprescribing_page.dart
+++ lib/screens/desprescribing/desprescribing_page.dart
@@ -1,97 +1,104 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mpibrasil/assets.dart';
 import 'package:mpibrasil/constants.dart';
 import 'package:mpibrasil/generated/l10n.dart';
+import 'package:mpibrasil/models/prompt.dart';
 import 'package:mpibrasil/screens/desprescribing/prompt_page.dart';
 import 'package:mpibrasil/screens/desprescribing/result_page.dart';
 
 class DesprescribingPage extends StatelessWidget {
   const DesprescribingPage({super.key});
 
+  Future<List<Prompt>> fetchPrompts() async {
+    final String response = await rootBundle.loadString(
+      MpiAssets.despPromptsJson,
+    );
+
+    final data = json.decode(response);
+    return (data['conditions'] as List).map((e) => Prompt.fromJson(e)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final Future<List<Prompt>> prompts = fetchPrompts();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(S.current.desprescribingPageTitle),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              S.current.desprescribingPageContent,
-              textAlign: TextAlign.justify,
-            ),
-            Expanded(child: Container()),
-
-            // begin prompts
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DespPromptPage(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25.0),
+                child: Image.asset(
+                  MpiAssets.imgUndrawDoctors,
+                  height: size.height * 0.20,
                 ),
               ),
-              child: Text(S.current.desprescribingStart),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorMPIGreen,
-                foregroundColor: kColorMPIWhite,
-              ),
-            ),
 
-            // todo: remove before feature launch
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DespResultPage(
-                    bgColor: kColorMPIGreenOpaque,
-                    resultTitle: S.current.considerNotDeprescribingTitle,
-                    resultContentList: S.current.considerNotDeprescribingList,
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25.0),
+                child: Text(
+                  S.current.despPageTitle,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
                 ),
               ),
-              child: Text(S.current.considerNotDeprescribingTitle),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorMPIGreen,
-                foregroundColor: kColorMPIWhite,
-              ),
-            ),
 
-            // todo: remove before feature launch
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DespResultPage(
-                    bgColor: kColorMPIOrange,
-                    resultTitle: S.current.considerDeprescribingTitle,
-                    resultContentList: S.current.considerDeprescribingList,
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25.0),
+                child: Text(
+                  S.current.despPageContent,
+                  textAlign: TextAlign.justify,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
                 ),
               ),
-              child: Text(S.current.considerDeprescribingTitle),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorMPIGreen,
-                foregroundColor: kColorMPIWhite,
-              ),
-            ),
 
-            // todo: remove before feature launch
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DespResultPage(
-                    bgColor: kColorMPIGreenOpaque,
-                    resultTitle: S.current.howToDeprescribeTitle,
-                    resultContentList: S.current.howToDeprescribeList,
-                  ),
+              // begin prompts
+              ElevatedButton(
+                onPressed: () async {
+                  final promptList = await prompts;
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => DespPromptPage(
+                        prompt: promptList[Random().nextInt(9)],
+                      ),
+                    ),
+                  );
+                },
+                child: Text(S.current.despButtonStart),
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(fontWeight: FontWeight.bold),
+                  backgroundColor: kColorMPIGreen,
+                  foregroundColor: kColorMPIWhite,
                 ),
               ),
-              child: Text(S.current.howToDeprescribeTitle),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorMPIGreen,
-                foregroundColor: kColorMPIWhite,
+
+              // back to app
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text(S.current.back),
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(fontWeight: FontWeight.bold),
+                  backgroundColor: kColorMPIGray,
+                  foregroundColor: kColorMPIWhite,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
