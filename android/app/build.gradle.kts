f2f0dad2d50343a5a27c8f68bbb6b399650e44d1
Diego Porto <diego.cfporto@gmail.com>
2025-11-15 16:00:22 -0300

    fix: remove dup debug config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,7 +48,6 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
             signingConfig = signingConfigs.getByName("release")
         }
     }
