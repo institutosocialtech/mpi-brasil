440f5256b5d6204168b97be6d85349c8eb14eed0
Diego Porto <diego.cfporto@gmail.com>
2025-11-15 16:08:43 -0300

    refactor: update android build system (#141)
    * chore: update README `BR` and `EN`
    
    * chore: update `dart sdk`, `flutter_mailer` and `intl`
    
    * fix: refactor kotlin build gradle
    
    * fix: gradle jvm properties
    
    * fix: update gradle wrapper properties
    
    * fix: refactor kotlin gradle settings
    
    * fix: refactor kotlin app build gradle
    
    * fix: remove dup debug config
    
    * chore: bump version to 2.2.4

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,62 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+// load keystore
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "br.org.socialtech.mpibrasil"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "br.org.socialtech.mpibrasil"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) } 
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+dependencies {
+    implementation("com.google.crypto.tink:tink-android:1.9.0")
+}
+
+flutter {
+    source = "../.."
+}
