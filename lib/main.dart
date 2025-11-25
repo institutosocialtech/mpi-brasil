13a9a848527defbda9e3b0eeefe7d0736dd2e452
bbering <bbering88@gmail.com>
2025-11-25 13:47:12 -0300

    feat: adicionada inicializacao do firebase na main e arquivos de config/dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
 import 'screens/about.dart';
 import 'screens/faq.dart';
 import 'screens/favorites.dart';
@@ -9,10 +12,15 @@ import 'screens/medicamento_info.dart';
 import 'screens/medicamentos.dart';
 import 'screens/settings.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // inicializa Firebase
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,17 +31,46 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Nunito',
       ),
       initialRoute: '/',
-      routes: <String,WidgetBuilder> {
-        '/'             : (context) => HomePage(),
-        '/about'        : (context) => About(),
-        '/login'        : (context) => Login(),
-        '/favorites'    : (context) => Favorites(),
-        '/faq'          : (context) => FAQ(),
-        '/glossario'    : (context) => Glossario(),
-        '/medicamentos' : (context) => Medicamentos(),
-        '/med_info'     : (context) => MedicamentoInfo(),
-        '/settings'     : (context) => Settings(),
+      routes: <String, WidgetBuilder>{
+        '/': (context) => HomePageWrapper(),
+        '/about': (context) => About(),
+        '/login': (context) => Login(),
+        '/favorites': (context) => Favorites(),
+        '/faq': (context) => FAQ(),
+        '/glossario': (context) => Glossario(),
+        '/medicamentos': (context) => Medicamentos(),
+        '/med_info': (context) => MedicamentoInfo(),
+        '/settings': (context) => Settings(),
       },
     );
+  }
+}
+
+/// wrapper para medir tempo de inicio da homepage (semi funcional)
+class HomePageWrapper extends StatefulWidget {
+  @override
+  _HomePageWrapperState createState() => _HomePageWrapperState();
+}
+
+class _HomePageWrapperState extends State<HomePageWrapper> {
+  Stopwatch _stopwatch = Stopwatch()..start();
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) async {
+      _stopwatch.stop();
+      await MyApp.analytics.logEvent(
+        name: 'home_load_time',
+        parameters: {'milliseconds': _stopwatch.elapsedMilliseconds},
+      );
+      print('Home load time: ${_stopwatch.elapsedMilliseconds}ms'); // debug
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
   }
 }
